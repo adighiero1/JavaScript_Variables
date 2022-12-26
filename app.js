fbfc21e3dbb7d113a9ea4caa007979f5040db9ef
Alejandro Dighiero <adighiero1@gmail.com>
2022-12-26 13:02:23 -0500

    Alejandro Excercise

--- app.js
+++ app.js
@@ -4,7 +4,27 @@ console.log(
 );
 
 // Exercise 1
-
+const firstName = "Alejandro";
+let lastName = "Dighiero";
+var age = 31;
 // Exercise 2
 
 // Exercise 3
+let language = "Javascript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+//Excercise 4
+let price = 19.99;
+let isOnSale = fale;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+//Excercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = "722";
+let bookmark = "456";
+let hasRead = true;
