9bf64eb42a2d0dcef4b10ce94f6b8ec6c69cd077
Alejandro Dighiero <adighiero1@gmail.com>
2023-04-30 17:20:32 -0400

    fixed

--- app.js
+++ app.js
@@ -13,18 +13,28 @@ var age = 31;
 let language = "Javascript";
 let createdYear = 1995;
 let isCaseSensitive = true;
-
+console.log(language + " was first released in " + createdYear);
 //Excercise 4
 let price = 19.99;
-let isOnSale = fale;
+let isOnSale = false;
 let salePercentage = 15;
 let stock = 0;
 let inStock = false;
 let selectedSize = "M";
-
+let product = "shorts";
+console.log(
+  product +
+    " in size " +
+    selectedSize +
+    " is " +
+    price +
+    " with " +
+    stock +
+    " in stock"
+);
 //Excercise 5
 let title = "Name of the Wind";
 let author = "Patrick Rothfuss";
-let pageCount = "722";
-let bookmark = "456";
+let pageCount = 722;
+let bookmark = 456;
 let hasRead = true;
